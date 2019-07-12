6f399325ecbf5417a17377147bd1de8d6b1ef407
shegilo <ilorioluwasegun@gmail.com>
2019-07-12 02:40:55 +0100

    update

---
+++ app/src/main/java/com/example/changeme/kitchenapp/Model/Token.java
@@ -0,0 +1,35 @@
+package com.example.changeme.kitchenapp.Model;
+
+/**
+ * Created by SHEGZ on 1/4/2018.
+ */
+public class Token {
+    private String token;
+    private boolean isServerToken;
+
+    public Token(){
+
+    }
+    public Token(String token, boolean isServerToken) {
+
+        this.token = token;
+        this.isServerToken = isServerToken;
+    }
+
+    public boolean isServerToken() {
+        return isServerToken;
+    }
+
+    public void setServerToken(boolean serverToken) {
+        isServerToken = serverToken;
+    }
+
+    public String getToken() {
+
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
